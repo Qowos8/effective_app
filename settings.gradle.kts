fa7a3d7bbf1e7da54286f8327ebaa07581895ed7
Qowos8 <namsr37@gmail.com>
2024-08-22 18:58:19 +0300

    1

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Effective_app"
+include(":app")
+include(":core_network")
+include(":core_domain")
+include(":feature_auth")
